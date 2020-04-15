d0b320aad89f2dd69d6522f0563011470167ab5d
Antony Noyelle <anto75019@gmail.com>
2020-04-15 16:12:12 +0200

    debut test API + mongoose

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
 var restify = require('restify');
 
+require('./resqest')
+require('./bdd')
+
 function respond(req, res, next) {
   res.send('hello ' + req.params.name);
   next();
 }
+
+
 
 var server = restify.createServer();
 server.get('/hello/:name', respond);
